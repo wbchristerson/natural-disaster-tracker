e995e369f9a28338c154507cbe8621215cd9c886
Will Christerson <wbchristerson@gmail.com>
2020-11-18 06:23:06 -0500

    chore: Add all files'

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export EXCITED=true
+# export DATABASE_URL='postgresql://will@localhost:5432/sample-capstoneDB'
+# export DATABASE_URL='postgres://will@localhost:5432/sample-capstoneDB'
+# export DATABASE_URL='sample-capstoneDB'
+export MY_DATABASE_URL='sample-capstoneDB'
