2a63adb73e1689f02e7c66491db042d82e9f61bd
Will Christerson <wbchristerson@gmail.com>
2020-11-19 06:21:31 -0500

    chore: Change setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ export EXCITED=true
 # export DATABASE_URL='postgres://will@localhost:5432/sample-capstoneDB'
 # export DATABASE_URL='sample-capstoneDB'
 export MY_DATABASE_URL='sample-capstoneDB'
+export DATABASE_URL='postgres://vghegyvjfifonz:0cd4f3798cd874cc0ed79a30d83dce2677ca79b6a3a507cc923574d5c80d9d4e@ec2-54-82-208-124.compute-1.amazonaws.com:5432/d7m5j8m4mo0oko'
