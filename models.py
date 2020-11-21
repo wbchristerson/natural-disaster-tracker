e54b3a9095fd4d562167c6a42b968467c42807ee
Will Christerson <wbchristerson@gmail.com>
2020-11-21 14:24:27 -0500

    feat: Implement witness report model

--- models.py
+++ models.py
@@ -1,14 +1,11 @@
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey, relationship,
+  Boolean, Enum, DateTime
 from flask_sqlalchemy import SQLAlchemy
 import json
 import os
+import enum
 
 database_path = os.environ['DATABASE_URL']
-# database_path = "postgres://{}:{}@{}/{}".format(os.environ.get('MY_PG_USER'), os.environ.get('MY_PG_PWD'), 'localhost:5432', os.environ['MY_DATABASE_URL'])
-
-# print("\n\ndatabase_path: ", database_path)
-# print("\n\n")
-
 db = SQLAlchemy()
 
 '''
@@ -23,21 +20,38 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 
+class NaturalDisasterEnum(enum.Enum):
+  earthquake = 1
+  flood = 2
+  wildfire = 3
+  tornado = 4
+  hurricane = 5
+  tsunami = 6
+  landslide = 7
+  avalanche = 8
+  other = 9
+
+
 '''
 Disaster
 '''
 class Disaster(db.Model):
-  __tablename__ = 'Disaster'
+  __tablename__ = 'disasters'
 
-  id = Column(db.Integer, primary_key=True)
+  id = Column(Integer, primary_key=True)
   informal_name = Column(String(50))
   official_name = Column(String(100))
-  disaster_type = Column(Integer)
+  disaster_type = Column('value', Enum(NaturalDisasterEnum))
+  witness_reports = relationship('WitnessReport', backref="witness_report_disaster",
+    lazy=True, collection_class=list, cascade="all, delete, delete-orphan")
+  is_ongoing = Column(Boolean, default=True)
+  # location
 
-  def __init__(self, informal_name, official_name, disaster_type):
+  def __init__(self, informal_name, official_name, disaster_type, is_ongoing):
     self.informal_name = informal_name
     self.official_name = official_name
     self.disaster_type = disaster_type
+    self.is_ongoing = is_ongoing
 
   def format(self):
     return {
@@ -45,6 +59,7 @@ class Disaster(db.Model):
       'informal_name': self.informal_name,
       'official_name': self.official_name,
       'disaster_type': self.disaster_type,
+      'is_ongoing': self.is_ongoing,
     }
 
 
@@ -52,11 +67,13 @@ class Disaster(db.Model):
 Observer
 '''
 class Observer(db.Model):
-  __tablename__ = 'Observer'
+  __tablename__ = 'observers'
 
-  id = Column(db.Integer, parimary_key=True)
-  username = Column(db.Integer, primary_key=True)
+  id = Column(Integer, parimary_key=True)
+  username = Column(Integer, primary_key=True)
   photograph_url = Column(String(120), nullable=True)
+  witness_reports = relationship('WitnessReport', backref="witness_report_observer",
+    lazy=True, collection_class=list, cascade="all, delete, delete-orphan")
 
   def __init__(self, username, photograph_url=None):
     self.username = username
@@ -68,3 +85,34 @@ class Observer(db.Model):
       'username': self.username,
       'photograph_url': self.photograph_url,
     }
+
+
+'''
+WitnessReport
+'''
+class WitnessReport(db.Model):
+  __tablename__ = 'WitnessReport'
+
+  id = Column(Integer, primary_key=True)
+  disaster_id = Column(Integer, ForeignKey('disasters.id'), nullable=False)
+  observer_id = Column(Integer, ForeignKey('observers.id'), nullable=False)
+  event_datetime = Column(DateTime, nullable=False)
+  severity = Column(Integer, nullable=True)
+  image_url = Column(String(120), nullable=True)
+  comment = Column(String(500), nullable=True)
+  people_affected = Column(Integer, default=0)
+  # location = Column(, nullable=False)
+
+  def __init__(self, disaster_id, observer_id, event_datetime):
+    self.disaster_id = disaster_id
+    self.observer_id = observer_id
+    self.event_datetime = event_datetime
+
+  def __repr__(self):
+    return {
+      'id': self.id,
+      'disaster_id': self.disaster_id,
+      'observer_id': self.observer_id,
+    }
+
+
