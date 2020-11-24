131c09109d683fd27f8ca2d01a8d90bc9ff93655
Will Christerson <wbchristerson@gmail.com>
2020-11-24 06:46:18 -0500

    feat: Begin disaster retrieval endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, abort, jsonify, flash
+from models import setup_db, Disaster, WitnessReport
 from flask_cors import CORS
+from sqlalchemy import func
+
 
 def create_app(test_config=None):
 
@@ -23,6 +25,35 @@ def create_app(test_config=None):
     return app
 
 app = create_app()
+
+
+'''
+A GET endpoint to get all disasters. This endpoint takes no parameters. This endpoint does
+not return any of the witness reports associated with a specific disaster. For each
+disaster, the data in the disaster table is returned along with a random comment and the
+author of that comment from a witness of the disaster (if any) and some descriptive
+data about the disaster reports per disaster (namely, the number of reports, the first
+observance, the last observance, and the number of people affected).
+'''
+@app.route('/disasters')
+def disasters():
+    try:
+        disasters = Disaster.query.all()
+        formatted_disasters = [disaster.format() for disaster in disasters]
+
+        list_people_affected = []
+        # list_people_affected = WitnessReport.query.groupby(WitnessReport.disaster_id).all()
+        # list_people_affected = WitnessReport.query(func.max(WitnessReport.people_affected).label('people_affected'), WitnessReport.disaster_id).groupby(WitnessReport.disaster_id).all()
+
+        return jsonify({
+            'disasters': formatted_disasters,
+            'list_people_affected': list_people_affected,
+        })
+    except Exception as ex:
+        flash('An error occurred.')
+        abort(404)
+
+
 
 if __name__ == '__main__':
     app.run()
