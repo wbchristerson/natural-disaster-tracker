1a2c90b34decfd7b6dd4527fca3e7b81262c96f4
Will Christerson <wbchristerson@gmail.com>
2021-01-01 06:31:32 -0500

    test: Try re-setting build directory location

--- app/__init__.py
+++ app/__init__.py
@@ -28,10 +28,11 @@ def create_app(test_config=None):
     directory_up_two_levels = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     app = Flask(__name__, static_folder=(directory_up_two_levels + '/client/build'), static_url_path='')
+    # app = Flask(__name__, static_folder='client/build', static_url_path='')
     setup_db(app)
     CORS(app, resources={
          r"*": {"origins": ["127.0.0.1",
-                            "127.0.0.1/api/"
+                            "127.0.0.1/api/",
                             "https://sample-will.herokuapp.com/",
                             "https://sample-will.herokuapp.com/api"]}})
 
@@ -39,6 +40,8 @@ def create_app(test_config=None):
     def serve():
         # return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         return send_from_directory(app.static_folder, 'index.html')
+        # return app.static_folder
+        # return "hello"
 
 
     @app.route('/api')
