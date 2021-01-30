a1354888d3884b6142eb251ebcc0d4268f87bf49
Will Christerson <wbchristerson@gmail.com>
2021-01-30 08:56:49 -0500

    fix: reset index directory send

--- app/__init__.py
+++ app/__init__.py
@@ -96,8 +96,8 @@ def create_app(test_config=None):
             # return send_from_directory(my_path, 'index.html')
 
             # return send_from_directory(app.static_folder, 'index.html', userinfo=session["profile"], userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
-            # return send_from_directory(app.static_folder, 'index.html')
-            return render_template('../client/build/index.html')
+            return send_from_directory(app.static_folder, 'index.html')
+            # return render_template('../client/build/index.html')
 
 
         except Exception as ex:
